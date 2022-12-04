d63999b9373d9b471cb133aae5a9df45f9d2af67
Pedro Lucas <pedroca242@gmail.com>
2022-12-04 16:27:15 -0300

    Day 4

---
+++ 2022/Day4.py
@@ -0,0 +1,22 @@
+
+def result(file):
+    count = 0
+    for f in file:
+        f = f.replace('\n', '')
+        interval1, interval2 = f.split(',')
+        interval1 = interval1.split('-')
+        interval2 = interval2.split('-')
+
+        n1 = [i for i in range(int(interval1[0]), int(interval1[-1])+1)]
+        n2 = [i for i in range(int(interval2[0]), int(interval2[-1])+1)]
+
+        check = [n for n in n1 if n in n2]
+
+        if len(check) != 0:
+            count += 1
+
+    return count
+
+file = open('Day4Input.txt')
+print(result(file))
+
