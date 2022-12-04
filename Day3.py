649fab30ef20585dc273d1f590f550046bce3c64
Pedro Lucas <pedroca242@gmail.com>
2022-12-04 15:40:59 -0300

    Adding functions

--- Day3.py
+++ Day3.py
@@ -1,60 +1,57 @@
-class Day3:
-    def __init__(self, file):
-        self.file = file
 
-    def partone(self):
-        minusculos = 'abcdefghijklmnopqrstuvwxyz'
-        maiusculos = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        prio = 0
+def partone(file):
+    minusculos = 'abcdefghijklmnopqrstuvwxyz'
+    maiusculos = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    prio = 0
 
-        for f in file:
-            letras = str(f)
-            primeira = letras[0:int(len(letras)/2)]
-            segunda = letras[int(len(letras)/2):len(letras)]
+    for f in file:
+        letras = str(f)
+        primeira = letras[0:int(len(letras)/2)]
+        segunda = letras[int(len(letras)/2):len(letras)]
 
-            semelhantes = []
-            for i in primeira:
-                for j in segunda:
-                    if i == j and i not in semelhantes:
-                        semelhantes.append(i)
+        semelhantes = []
+        for i in primeira:
+            for j in segunda:
+                if i == j and i not in semelhantes:
+                    semelhantes.append(i)
+                else:
+                    pass
+        if semelhantes[0].isupper():
+            prio += (maiusculos.index(semelhantes[0]) + 27)
+        else:
+            prio += (minusculos.index(semelhantes[0]) + 1)
+
+    return prio
+
+def parttwo(file):
+    content = file.readlines()
+    minusculos = 'abcdefghijklmnopqrstuvwxyz'
+    maiusculos = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    prio = 0
+
+    for i in range(2,len(content),3):
+        terceira = content[i]
+        segunda = content[i-1]
+        primeira = content[i-2]
+
+        semelhantes = []
+        for m in primeira:
+            for n in segunda:
+                for p in terceira:
+                    if m == n == p and m not in semelhantes and m != '\n':
+                        semelhantes.append(m)
                     else:
                         pass
-            if semelhantes[0].isupper():
-                prio += (maiusculos.index(semelhantes[0]) + 27)
-            else:
-                prio += (minusculos.index(semelhantes[0]) + 1)
 
-        return prio
+        if semelhantes[0].isupper():
+            prio += (maiusculos.index(semelhantes[0]) + 27)
+        else:
+            prio += (minusculos.index(semelhantes[0]) + 1)
 
-    def parttwo(self):
-        content = file.readlines()
-        minusculos = 'abcdefghijklmnopqrstuvwxyz'
-        maiusculos = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        prio = 0
-
-        for i in range(2,len(content),3):
-            terceira = content[i]
-            segunda = content[i-1]
-            primeira = content[i-2]
-
-            semelhantes = []
-            for m in primeira:
-                for n in segunda:
-                    for p in terceira:
-                        if m == n == p and m not in semelhantes and m != '\n':
-                            semelhantes.append(m)
-                        else:
-                            pass
-
-            if semelhantes[0].isupper():
-                prio += (maiusculos.index(semelhantes[0]) + 27)
-            else:
-                prio += (minusculos.index(semelhantes[0]) + 1)
-
-        return prio
+    return prio
 
 
 file = open('Day3Input')
-
-obj = Day3(file)
-print(obj.parttwo())
+print(partone(file))
+file = open('Day3Input')
+print(parttwo(file))
